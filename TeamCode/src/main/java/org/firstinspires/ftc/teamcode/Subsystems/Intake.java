9739e528c95ae8b98580040487cece4124250c94
Om Ramanathan <65601935+megabanana23@users.noreply.github.com>
2026-09-14 18:17:46 -0400

    Builds Intake Subsystem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Intake.java
@@ -1,7 +1,49 @@
 package org.firstinspires.ftc.teamcode.Subsystems;
 
-public class Intake {
+import com.pedropathing.ivy.Command;
+
+import dev.nextftc.hardware.actuators.NextMotor;
+import dev.nextftc.robot.Mechanism;
 
 
+public class Intake implements Mechanism {
+    public static  double intake = 1.0;
+    public static  double outtake = -1.0;
 
+    private final NextMotor motor;
+
+
+    public Intake(String motorName) {
+        motor = new NextMotor(motorName);
+        motor.setDirection(NextMotor.Direction.FORWARD);
+        motor.setZeroPowerBehavior(NextMotor.ZeroPowerBehavior.BRAKE);
+    }
+
+    public Intake() {
+        this("intakeMotor");
+    }
+
+
+    public Command intake() {
+        return runAt(intake);
+    }
+
+    public Command outtake() {
+        return runAt(outtake);
+    }
+
+    public Command stop() {
+        return instant(() -> motor.setThrottle(0.0));
+    }
+
+    @Override
+    public Command getDefaultCommand() {
+        return runAt(0.0);
+    }
+
+    public Command runAt(double power) {
+        return infinite(() -> motor.setThrottle(power))
+                .setStart(() -> motor.setThrottle(power))
+                .setEnd(condition -> motor.setThrottle(0.0));
+    }
 }
